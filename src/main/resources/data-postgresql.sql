b1edba7608421bc2f73571b494a3d98d6b4d06f4
Dean4iq <denis.ua22@gmail.com>
2019-08-14 15:53:50 +0300

    New commit

--- src/main/resources/data-postgresql.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public.authority VALUES (1, 'ROLE_ADMIN'), (2, 'ROLE_USER');
-INSERT INTO public.user VALUE ('admin', '$2y$11$vh8O5kLsk9Sfoo8rZFLnmukw2eKlQ2queg4KbnDacYnE5PFNIDjkq', 'ADMIN', 'ADMIN', '2000-01-01 00:00:00', true, '2000-01-01 00:00:00', 1);
