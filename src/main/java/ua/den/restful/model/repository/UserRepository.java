38c8b059c8e9dc43513de3a103938f028d91c2ab
Dean4iq <denis.ua22@gmail.com>
2019-08-16 13:09:35 +0300

    New day - new patch v3

--- src/main/java/ua/den/restful/model/repository/UserRepository.java
+++ src/main/java/ua/den/restful/model/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package ua.den.restful.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ua.den.restful.model.entity.Authority;
@@ -10,6 +11,7 @@ public interface UserRepository extends JpaRepository<User, String> {
     User getUserByLogin(String login);
     void deleteByAuthority(Authority authority);
 
+    @Modifying
     @Query(value = "UPDATE public.user SET enabled = :enabled WHERE authority = :authorityId", nativeQuery = true)
     void updateUsersByAuthority(@Param("enabled") boolean enabled, @Param("authorityId") int authorityId);
 }
