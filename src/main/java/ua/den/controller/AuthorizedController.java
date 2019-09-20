1da389e98233711604464c079f6bf1e68d56f5f0
Dean4iq <denis.ua22@gmail.com>
2019-09-20 13:52:00 +0300

    Implementation for support

--- src/main/java/ua/den/controller/AuthorizedController.java
+++ src/main/java/ua/den/controller/AuthorizedController.java
@@ -1,10 +1,16 @@
 package ua.den.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class AuthorizedController {
@@ -21,5 +27,29 @@ public class AuthorizedController {
     @GetMapping("accessDenied")
     public String getAccessDeniedPage(HttpServletResponse response) {
         return "authorized/access_denied";
+    }
+
+    @PostMapping(value = "support-output", produces = "application/json")
+    @ResponseBody()
+    public Map<String, String> manageSupportMessage(@RequestParam("name_inp") @NotBlank String name,
+                                                    @RequestParam("email_inp") @NotBlank String email,
+                                                    @RequestParam("subject_inp") @NotBlank String subject,
+                                                    @RequestParam("text_inp") @NotEmpty String text,
+                                                    BindingResult bindingResult) {
+        Map<String, String> message = new HashMap<>();
+
+        if (bindingResult.hasErrors()) {
+            List<FieldError> errors = bindingResult.getFieldErrors();
+
+            for (FieldError e : errors) {
+                message.put("@" + e.getField().toUpperCase(), e.getDefaultMessage());
+            }
+
+            message.put("status", "input_errors");
+            return message;
+        }
+
+        message.put("status", "success");
+        return message;
     }
 }
