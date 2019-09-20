b63fc4c2f41c9a6fdd88e93de279860fca018c11
Dean4iq <denis.ua22@gmail.com>
2019-09-20 15:45:11 +0300

    Added new Validation process inside DTO for support form

--- src/main/java/ua/den/controller/AuthorizedController.java
+++ src/main/java/ua/den/controller/AuthorizedController.java
@@ -1,16 +1,25 @@
 package ua.den.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.DataBinder;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import ua.den.model.dto.UserApplySupportDto;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Controller
 public class AuthorizedController {
@@ -30,17 +39,29 @@ public class AuthorizedController {
     }
 
     @GetMapping("support")
-    public String getSupportPage() {
-        return "authorized/support";
+    public ModelAndView getSupportPage() {
+        ModelAndView modelAndView = new ModelAndView("authorized/support");
+
+        modelAndView.addObject("userApplySupportDto", new UserApplySupportDto());
+
+        return modelAndView;
     }
 
     @RequestMapping(value = "support-output", produces = "application/json", method = RequestMethod.POST)
     @ResponseBody()
-    public Map<String, String> manageSupportMessage(@RequestParam("name_sender") @NotBlank String name,
-                                                    @RequestParam("email_sender") @NotBlank String email,
-                                                    @RequestParam("subject") @NotBlank String subject,
-                                                    @RequestParam("text") @NotEmpty String text) {
+    public Map<String, String> manageSupportMessage(UserApplySupportDto userApplySupportDto) {
         Map<String, String> message = new HashMap<>(1);
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<UserApplySupportDto>> violations = validator.validate(userApplySupportDto);
+
+        if (violations.size() != 0) {
+            for (ConstraintViolation<UserApplySupportDto> violation : violations) {
+                message.put(violation.getPropertyPath().toString(), violation.getMessage());
+            }
+
+            message.put("status", "input_errors");
+            return message;
+        }
 
         message.put("status", "success");
         return message;
---
+++ src/main/java/ua/den/model/dto/UserApplySupportDto.java
@@ -0,0 +1,68 @@
+package ua.den.model.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class UserApplySupportDto implements Serializable {
+    private static final long serialVersionUID = 8927423443534534211L;
+
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String email;
+    @NotNull
+    private String subject;
+    @NotEmpty
+    private String text;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserApplySupportDto that = (UserApplySupportDto) o;
+        return name.equals(that.name) &&
+                email.equals(that.email) &&
+                subject.equals(that.subject) &&
+                text.equals(that.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email, subject, text);
+    }
+}
