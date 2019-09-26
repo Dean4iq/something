9d7bfd86569e1879eed6707a03633c79dfd6d68c
Dean4iq <denis.ua22@gmail.com>
2019-09-26 16:47:48 +0300

    Bug with null displaying time fixing

--- src/main/java/ua/den/model/entity/NewsXML.java
+++ src/main/java/ua/den/model/entity/NewsXML.java
@@ -36,13 +36,7 @@ public class NewsXML implements Serializable {
     }
 
     private boolean isNewsDisplayable(Date date) {
-        System.out.println("==!==");
-        System.out.println(date);
-        System.out.println(date.toString());
-        System.out.println(new Date().toString());
-        System.out.println(toBeDisplayed.toString());
-        System.out.println("==========");
-        return date.compareTo(toBeDisplayed) > 0;
+        return (toBeDisplayed == null) || (date.compareTo(toBeDisplayed) > 0);
     }
 
     public String getHeader() {
