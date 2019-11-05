c6c7164bc7d621d62571e29031e22b7ac3ea91ff
Dean4iq <denis.ua22@gmail.com>
2019-11-05 15:11:10 +0200

    new commit for local

---
+++ src/main/java/ua/den/configuration/DataSourceConfig.java
@@ -0,0 +1,23 @@
+package ua.den.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Value("${spring.datasource.url}")
+    private String databaseUrl;
+
+    @Bean
+    public DataSource getDataSource() {
+        databaseUrl = databaseUrl.replaceAll("\"","");
+
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(databaseUrl);
+        return dataSourceBuilder.build();
+    }
+}
